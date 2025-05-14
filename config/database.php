1a195517d0ba4fadf0c4afb0c71265fe62a94717
elchimeneas <cesramluc@gmail.com>
2025-05-14 16:37:12 +0100

    Connected to supabase and fixed css.

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 class Database
 {
-  private $host = "localhost";
-  private $db_name = "ticketing_system";
-  private $username = "root";
-  private $password = "";
+  private $host = "aws-0-eu-west-2.pooler.supabase.com";
+  private $db_name = "postgres";
+  private $username = "postgres.auktrmhxykcwiprrtmzr";
+  private $password = "alumno";
+  private $port = 6543;
   public $conn;
 
   public function getConnection()
@@ -12,8 +13,8 @@ class Database
     $this->conn = null;
 
     try {
-      $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-      $this->conn->exec("set names utf8");
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+      $this->conn = new PDO($dsn, $this->username, $this->password);
     } catch (PDOException $exception) {
       echo "Error de conexión: " . $exception->getMessage();
     }
--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@ $settings = $allSettings->read();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="./assets/img/favicon/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="assets/css/styles.css">
   <title><?php if ($settings[0]['site_name']): ?><?php echo $settings[0]['site_name']; ?><?php else: ?>Ticketing Support System<?php endif; ?></title>
 
   <!-- Estilos personalizados -->
